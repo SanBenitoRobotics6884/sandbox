e8bd4b8c0f644959bfc3bdbf1cb1bb529f1af90b
ROBOTICS-03 <ROBOTICS-03@sbhsd.k12.ca.us>
2023-03-04 13:44:18 -0800

    first commit

---
+++ DemoBotDriveExample/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,117 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  XboxController m_controller = new XboxController(1);
+
+  WPI_TalonSRX m_frontLeft = new WPI_TalonSRX(1);
+  WPI_TalonSRX m_backLeft = new WPI_TalonSRX(2);
+  WPI_VictorSPX m_frontRight = new WPI_VictorSPX(3);
+  WPI_VictorSPX m_backRight = new WPI_VictorSPX(4);
+
+
+  DifferentialDrive m_arcDrive = new DifferentialDrive(m_frontLeft, m_frontRight);
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_backLeft.configFactoryDefault();
+    m_backRight.configFactoryDefault();
+    m_frontLeft.configFactoryDefault();
+    m_frontRight.configFactoryDefault();
+    m_backRight.follow(m_frontRight);
+    m_backLeft.follow(m_frontLeft);
+    m_frontRight.setInverted(true);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    double forw = m_controller.getLeftY();
+    double turn = m_controller.getRightX();
+
+    if (Math.abs(forw) < 0.10) {
+      forw = 0;
+    }
+    if (Math.abs(turn) < 0.10) {
+      turn = 0;
+    }
+    m_arcDrive.arcadeDrive(forw, turn);
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
